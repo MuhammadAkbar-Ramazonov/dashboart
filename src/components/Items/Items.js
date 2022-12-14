581db61536e16040ea0cafa6b6492bc34f99dc46
Muhammad Akbar <ramazonov3600@gmail.com>
2022-12-15 02:17:50 +0500

    site finish

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,6 +1,6 @@
 export function Items(children) {
 	return children.obg.map((item) => (
-		<li className='d-flex site-nav-item align-items-center'>
+		<li key={item.id} className='d-flex site-nav-item align-items-center'>
 			{item.img}
 			<p className='mb-0 ms-4 site-nav-text'>{item.text}</p>
 		</li>
